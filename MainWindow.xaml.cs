f90ae03c8d5899bf3e6754c894f5cb7b63506fe9
Kirk Francis Gonzalez <45276166+KirkFrancisGonzalez@users.noreply.github.com>
2018-12-10 23:20:21 +0800

    Added ExitButton
    added close button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,5 +38,10 @@ namespace OOP_PROJECT_tinood_
             jew.Show();
             this.Close();
         }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
